a7e58dd74676dde6d8cb439e98d0f74e7833cd63
Richard Liu <ricl@microsoft.com>
2016-12-03 23:42:12 +0800

    added s3 to sum of two intergers.

---
+++ Leet Code/Cpp/Sum of Two Integers/s3.cpp
@@ -0,0 +1,35 @@
+// https://discuss.leetcode.com/topic/49771/java-simple-easy-understand-solution-with-explanation/2
+// Iterative
+public int getSum(int a, int b) {
+	if (a == 0) return b;
+	if (b == 0) return a;
+
+	while (b != 0) {
+		int carry = a & b;
+		a = a ^ b;
+		b = carry << 1;
+	}
+	
+	return a;
+}
+
+// Iterative
+public int getSubtract(int a, int b) {
+	while (b != 0) {
+		int borrow = (~a) & b;
+		a = a ^ b;
+		b = borrow << 1;
+	}
+	
+	return a;
+}
+
+// Recursive
+public int getSum(int a, int b) {
+	return (b == 0) ? a : getSum(a ^ b, (a & b) << 1);
+}
+
+// Recursive
+public int getSubtract(int a, int b) {
+	return (b == 0) ? a : getSubtract(a ^ b, (~a & b) << 1);
+}
