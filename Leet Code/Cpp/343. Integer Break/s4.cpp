f2a3f917ab2ad9c26b9cee1765f0ae5a483ad202
Richard LIU <liuzhenglaichn@gmail.com>
2016-12-14 22:59:21 +0800

    343 | Integer Break | Medium (update)

---
+++ Leet Code/Cpp/343. Integer Break/s4.cpp
@@ -0,0 +1,15 @@
+// https://discuss.leetcode.com/topic/45341/a-simple-explanation-of-the-math-part-and-a-o-n-solution
+class Solution {
+public:
+    int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int p = 1;
+        while (n > 4) {
+            p *= 3;
+            n -= 3;
+        }
+        p *= n;
+        return p;
+    }
+};
