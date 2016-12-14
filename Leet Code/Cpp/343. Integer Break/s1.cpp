89ecb0100d928fd17e2a07c41581122203d1a3fc
Richard LIU <liuzhenglaichn@gmail.com>
2016-12-14 20:54:45 +0800

    343 | Integer Break | Medium

---
+++ Leet Code/Cpp/343. Integer Break/s1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+    int dp[59];
+public:
+    int integerBreak(int n) {
+        for (int i = 0; i < 59; ++i) {
+            dp[i] = 1;
+        }
+        for (int i = 1; i < n; ++i) {
+            for (int j = i; j <= n; ++j) {
+                dp[j] = max(dp[j], dp[j - i] * i);
+            }
+        }
+        return dp[n];
+    }
+};
