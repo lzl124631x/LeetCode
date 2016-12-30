fc5c9ac049f490d3e712e9c1064cfee515b99fe7
Richard LIU <liuzhenglaichn@gmail.com>
2016-12-30 20:10:56 +0800

    472 | Concatenated Words | Hard

---
+++ Leet Code/Cpp/472. Concatenated Words/s2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        unordered_set<string> s(words.begin(), words.end());
+        vector<string> v;
+        for (string word : words) {
+            int n = word.size();
+            vector<bool> dp(n, false);
+            for (int i = 0; i < n - 1; ++i) {
+                if (s.count(word.substr(0, i + 1))) dp[i] = true;
+                if (!dp[i]) continue;
+                for (int j = i + 1; j < n; ++j) {
+                    if (s.count(word.substr(i + 1, j - i))) dp[j] = true;
+                }
+                if (dp[n - 1]) {
+                    v.push_back(word);
+                    break;
+                }
+            }
+        }
+        return v;
+    }
+};
