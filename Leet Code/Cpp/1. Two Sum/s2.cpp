806dfe72ff458f9ca4404815425806aeab01cdc0
Richard LIU <liuzhenglaichn@gmail.com>
2017-01-03 00:13:38 +0800

    minor change.

--- Leet Code/Cpp/1. Two Sum/s2.cpp
+++ Leet Code/Cpp/1. Two Sum/s2.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int, int> m;
         for (int i = 0; i < nums.size(); ++i) {
-            if (m.count(target - nums[i]) && i != m[target - nums[i]]) {
+            if (m.count(target - nums[i])) {
                 return { i, m[target - nums[i]] };
             }
             m[nums[i]] = i;
