04e47e880520467ec0f58a9756b22bece6068f49
Richard LIU <liuzhenglaichn@gmail.com>
2017-01-03 00:12:21 +0800

    1 | Two Sum | Easy

---
+++ Leet Code/Cpp/1. Two Sum/s1.cpp
@@ -0,0 +1,21 @@
+static bool cmp (pair<int, int> a, pair<int, int> b) {
+    return a.first < b.first;
+}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int, int>> v;
+        for (int i = 0; i < nums.size(); ++i) v.push_back(make_pair(nums[i], i));
+        sort(v.begin(), v.end(), cmp);
+        int i = 0, j = v.size() - 1;
+        while (i < j) {
+            int sum = v[i].first + v[j].first;
+            if (sum > target) --j;
+            else if (sum < target) ++i;
+            else {
+                return { v[i].second, v[j].second };
+            }
+        }
+        return {};
+    }
+};
