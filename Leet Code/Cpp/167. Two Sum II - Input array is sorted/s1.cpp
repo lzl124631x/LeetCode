65035ad8a6e222c12f68749d6bdc94fcb9778f34
Richard LIU <liuzhenglaichn@gmail.com>
2017-01-03 00:19:36 +0800

    167 | Two Sum II - Input array is sorted | Medium

---
+++ Leet Code/Cpp/167. Two Sum II - Input array is sorted/s1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i = 0, j = nums.size() - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] > target) --j;
+            else if (nums[i] + nums[j] < target) ++i;
+            else return { i + 1, j + 1 };
+        }
+        return {};
+    }
+};
--- README.md
+++ README.md
@@ -74,3 +74,4 @@ My Code for Online Judges.
 398 | Random Pick Index | Medium
 442 | Find All Duplicates in an Array | Medium
 1 | Two Sum | Easy
+167 | Two Sum II - Input array is sorted | Medium
