b9ab417236adc1ae966463bd0f95da9ba4d45621
Richard LIU <liuzhenglaichn@gmail.com>
2017-01-07 17:37:25 +0800

    84 | Largest Rectangle in Histogram | Medium

---
+++ Leet Code/Cpp/127. Word Ladder/s1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    void addWords(string &word, unordered_set<string> &wordList, queue<string> &q) {
+        for (int i = 0; i < word.size(); ++i) {
+            char c = word[i];
+            for (int j = 0; j < 26; ++j) {
+                word[i] = 'a' + j;
+                if (wordList.find(word) != wordList.end()) {
+                    q.push(word);
+                    wordList.erase(word);
+                }
+            }
+            word[i] = c;
+        }
+    }
+public:
+    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
+        queue<string> q;
+        int d = 1;
+        q.push(beginWord);
+        wordList.insert(endWord);
+        while (!q.empty()) {
+            int n = q.size();
+            while (n--) {
+                string s = q.front();
+                q.pop();
+                if (s == endWord) return d;
+                addWords(s, wordList, q);
+            }
+            ++d;
+        }
+        return 0;
+    }
+};
