a568715a42a108d58d6f91cb2e1de60e89d04e08
Richard LIU <liuzhenglaichn@gmail.com>
2017-01-08 18:28:26 +0800

    482 | License Key Formatting | Medium; 15 | 3Sum | Medium

---
+++ Leet Code/Cpp/15. 3Sum/s1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        if (nums.size() < 3) return {};
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ret;
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            int L = i + 1, R = nums.size() - 1;
+            while (L < R) {
+                while (L < R) {
+                    int sum = nums[i] + nums[L] + nums[R];
+                    if (!sum) {
+                        vector<int> v { nums[i], nums[L++], nums[R--] };
+                        ret.push_back(v);
+                        while (L < R && nums[L] == nums[L - 1]) ++L;
+                        while (L < R && nums[R] == nums[R + 1]) --R;
+                    } else if (sum > 0) --R;
+                    else ++L;
+                }
+            }
+            while (i + 1 < nums.size() - 2 && nums[i + 1] == nums[i]) ++i;
+        }
+        return ret;
+    }
+};
