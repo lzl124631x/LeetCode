7e7f74b6d799c5b7f451c80a4f4f5f1f01ce26a8
Richard LIU <liuzhenglaichn@gmail.com>
2017-01-11 00:42:34 +0800

    72 | Edit Distance | Hard

---
+++ Leet Code/Cpp/72. Edit Distance/s2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int M = word1.size(), N = word2.size();
+        if (M < N) {
+            swap(word1, word2);
+            swap(M, N);
+        }
+        vector<int> dp(N + 1, 0);
+        for (int i = 1; i <= N; ++i) dp[i] = i;
+        for (int i = 1; i <= M; ++i) {
+            int pre = dp[0];
+            dp[0] = i;
+            for (int j = 1; j <= N; ++j) {
+                int tmp = dp[j];
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[j] = pre;
+                } else {
+                    dp[j] = min(pre, min(dp[j - 1], dp[j])) + 1;
+                }
+                pre = tmp;
+            }
+        }
+        return dp[N];
+    }
+};
