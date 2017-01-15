ff5a67b6f2199904dd821757dd32045a720ed14b
Richard LIU <liuzhenglaichn@gmail.com>
2017-01-15 23:25:49 +0800

    173 | Binary Search Tree Iterator | Medium; 257 | Binary Tree Paths | Easy

---
+++ Leet Code/Cpp/173. Binary Search Tree Iterator/s1.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+private:
+    stack<TreeNode*> s;
+    void pushNode(TreeNode *root) {
+        while (root) {
+            s.push(root);
+            root = root->left;
+        }
+    }
+public:
+    BSTIterator(TreeNode *root) {
+        pushNode(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !s.empty();
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *root = s.top();
+        s.pop();
+        pushNode(root->right);
+        return root->val;
+    }
+};
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = BSTIterator(root);
+ * while (i.hasNext()) cout << i.next();
+ */
