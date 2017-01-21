d31cd267b9f5b77d953c5ab9686c67d364d868d8
Richard LIU <liuzhenglaichn@gmail.com>
2017-01-21 19:03:06 +0800

    78 | Subsets | Medium

---
+++ leetcode/78. Subsets/s2.cpp
@@ -0,0 +1,14 @@
+// https://discuss.leetcode.com/topic/2764/my-solution-using-bit-manipulation
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int N = 1 << nums.size();
+        vector<vector<int>> ans(N, vector<int>());
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = 0; j < N; ++j) {
+                if ((j >> i) & 1) ans[j].push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
