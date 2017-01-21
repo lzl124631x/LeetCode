dd79cd26bfd676a10f51f785bc8dd5a81351aef6
Richard LIU <liuzhenglaichn@gmail.com>
2017-01-21 11:24:01 +0800

    48 | Rotate Image | Medium

---
+++ leetcode/48. Rotate Image/s1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int N = matrix.size();
+        for (int i = 0; i < N / 2; ++i) {
+            for (int j = i; j < N - i - 1; ++j) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[N - j - 1][i];
+                matrix[N - j - 1][i] = matrix[N - i - 1][N - j - 1];
+                matrix[N - i - 1][N - j - 1] = matrix[j][N - i - 1];
+                matrix[j][N - i - 1] = tmp;
+            }
+        }
+    }
+};
