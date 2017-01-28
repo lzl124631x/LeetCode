33e66665edc4d90b16d0f7b9eb253ab3a7d1a290
Richard LIU <liuzhenglaichn@gmail.com>
2017-01-28 23:54:41 +0800

    222 | Count Complete Tree Nodes | Medium

--- leetcode/222. Count Complete Tree Nodes/s1.cpp
+++ leetcode/222. Count Complete Tree Nodes/s1.cpp
@@ -1,3 +1,12 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
  class Solution {
  public:
     int countNodes(TreeNode* root) {
