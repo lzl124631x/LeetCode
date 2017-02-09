ed24282455906a07df93c7457aa0f92190502ed9
Richard LIU <liuzhenglaichn@gmail.com>
2017-02-09 22:01:07 +0800

    328 | Odd Even Linked List | Medium

---
+++ leetcode/328. Odd Even Linked List/s2.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode *odd = head, *even = head->next, *evenHead = head->next;
+        while (even && even->next) {
+            odd->next = even->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
