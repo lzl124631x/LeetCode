97b8dc065b794a868167350cdf2290a54d495d11
Richard LIU <liuzhenglaichn@gmail.com>
2017-02-14 00:01:50 +0800

    403 | Frog Jump | Hard

--- leetcode/403. Frog Jump/s1.cpp
+++ leetcode/403. Frog Jump/s1.cpp
@@ -1,25 +1,22 @@
 class Solution {
 private:
-    bool rec(vector<int> &stones, vector<unordered_map<int, bool>> &dp, int start, int step) {
-        if (start == stones.size() - 1) {
-            return true;
-        }
-        if (dp[start].count(step)) return dp[start][step];
-        dp[start][step] = false;
-        int index = start + 1;
-        for (int i = -1; i <= 1; ++i) {
-            int target = stones[start] + step + i;
-            while (index < stones.size() && stones[index] < target) ++index;
-            if (stones[index] == target) {
-                dp[start][step] = dp[start][step] || rec(stones, dp, index, step + i);
+    bool jump(vector<int> &stones, vector<unordered_map<int, bool>> &memo, int start, int lastMove) {
+        if (start == stones.size() - 1) return true;
+        if (memo[start].count(lastMove)) return memo[start][lastMove];
+        int next = start + 1;
+        for (int d = -1; d <= 1; ++d) {
+            int move = lastMove + d;
+            while (next < stones.size() && stones[next] - stones[start] < move) ++next;
+            if (next < stones.size() && stones[next] - stones[start] == move && jump(stones, memo, next, move)) {
+                return memo[start][lastMove] = true;
             }
         }
-        return dp[start][step];
+        return memo[start][lastMove] = false;
     }
 public:
     bool canCross(vector<int>& stones) {
-        if (stones.size() < 2 || stones[0] != 0 || stones[1] != 1) return false;
-        vector<unordered_map<int, bool>> dp(stones.size());
-        return rec(stones, dp, 1, 1);
+        if (stones[1] != 1) return false;
+        vector<unordered_map<int, bool>> memo(stones.size());
+        return jump(stones, memo, 1, 1);
     }
 };
