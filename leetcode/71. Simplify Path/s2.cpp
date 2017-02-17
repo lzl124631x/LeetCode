fa754a9e56c0d9bed56f1fa639b13285771a4c0a
Richard LIU <liuzhenglaichn@gmail.com>
2017-02-17 13:08:47 +0800

    71 | Simplify Path | Medium

---
+++ leetcode/71. Simplify Path/s2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stringstream ss(path);
+        vector<string> s;
+        string name;
+        while (getline(ss, name, '/')) {
+            if (name == "..") {
+                if (s.size()) s.pop_back();
+            } else if (name.size() && name != ".") s.push_back(name);
+        }
+        string ans;
+        for (auto name : s) ans += '/' + name;
+        return ans.size() ? ans : "/";
+    }
+};
