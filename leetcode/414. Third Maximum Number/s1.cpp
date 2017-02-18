cd623e0f3465e65fbce70f5c4625128a76a30740
Richard LIU <liuzhenglaichn@gmail.com>
2017-02-19 00:34:56 +0800

    414 | Third Maximum Number | Easy

--- leetcode/414. Third Maximum Number/s1.cpp
+++ leetcode/414. Third Maximum Number/s1.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        int m[3] = { 0 };
-        bool b[3] = { false };
+        vector<long long> v(3, LONG_LONG_MIN);
         for (int n : nums) {
             int i = 0;
-            for (; i < 3 && b[i] && n < m[i]; ++i);
-            if (i < 3) {
-                if (b[i] && n > m[i]) {
-                    for (int j = 2; j > i; --j) {
-                        if (b[j - 1]) {
-                            m[j] = m[j - 1];
-                            b[j] = true;
-                        }
-                    }
-                }
-                b[i] = true;
-                m[i] = n;
-            }
+            while (i < 3 && v[i] > n) ++i;
+            if (i == 3 || v[i] == n) continue;
+            for (int j = 2; j > i; --j) v[j] = v[j - 1];
+            v[i] = n;
         }
-        return b[2] ? m[2] : m[0];
+        return v[2] == LONG_LONG_MIN ? v[0] : v[2];
     }
 };
