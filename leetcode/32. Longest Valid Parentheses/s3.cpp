dd14e6062b380b1caf6218b8cdca7864710870a4
Richard LIU <liuzhenglaichn@gmail.com>
2017-02-19 18:25:02 +0800

    32 | Longest Valid Parentheses | Hard

--- leetcode/32. Longest Valid Parentheses/s3.cpp
+++ leetcode/32. Longest Valid Parentheses/s3.cpp
@@ -3,15 +3,11 @@ public:
     int longestValidParentheses(string s) {
         stack<int> st;
         st.push(-1);
+        int ans = 0;
         for (int i = 0; i < s.size(); ++i) {
             if (s[i] == ')' && st.top() != -1 && s[st.top()] == '(') st.pop();
             else st.push(i);
-        }
-        int ans = 0, end = s.size();
-        while (st.size()) {
-            ans = max(ans, end - st.top() - 1);
-            end = st.top();
-            st.pop();
+            ans = max(ans, i - st.top());
         }
         return ans;
     }
