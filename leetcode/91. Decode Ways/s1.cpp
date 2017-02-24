3895f80f37a2d903ad74e15458d4f04162ff18ea
Richard LIU <liuzhenglaichn@gmail.com>
2017-02-24 17:48:57 +0800

    91 | Decode Ways | Medium

--- leetcode/91. Decode Ways/s1.cpp
+++ leetcode/91. Decode Ways/s1.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        if (s.empty()) return 0;
-        int pre2 = 1, pre1 = 1, cur = 0;
-        for (int i = 0; i < s.size() && pre1; ++i) {
-            if (s[i] != '0') cur += pre1;
-            int n = (s[i - 1] - '0') * 10 + s[i] - '0';
-            if (i != 0 && s[i - 1] != '0' && n <= 26) cur += pre2;
-            pre2 = pre1;
-            pre1 = cur;
-            cur = 0;
-        }
-        return pre1;
+  int numDecodings(string s) {
+    if (s.empty()) return 0;
+    int pre2 = 1, pre1 = 1, cur = 0;
+    for (int i = 0; i < s.size() && pre1; ++i) {
+      if (s[i] != '0') cur += pre1;
+      if (i != 0 && s[i - 1] != '0' && (s[i - 1] - '0') * 10 + s[i] - '0' <= 26)
+        cur += pre2;
+      pre2 = pre1;
+      pre1 = cur;
+      cur = 0;
     }
+    return pre1;
+  }
 };
