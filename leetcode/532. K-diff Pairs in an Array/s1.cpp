542c380a06d56f019dca5f26bd87cfc3b46137c1
Richard LIU <liuzhenglaichn@gmail.com>
2017-03-06 20:56:15 +0800

    532 | K-diff Pairs in an Array | Easy

--- leetcode/532. K-diff Pairs in an Array/s1.cpp
+++ leetcode/532. K-diff Pairs in an Array/s1.cpp
@@ -1,8 +1,11 @@
+// Author: https://github.com/lzl124631x
+// Time: O(N)
+// Space: O(N)
 class Solution {
 public:
   int findPairs(vector<int>& nums, int k) {
     if (k < 0) return 0;
-    map<int, int> m;
+    unordered_map<int, int> m;
     for (int n : nums) m[n]++;
       int cnt = 0;
     for (auto p : m) {
