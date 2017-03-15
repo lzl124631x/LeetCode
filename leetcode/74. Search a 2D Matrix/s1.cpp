322235b0e1e515022c9281804b10c68083823ea1
Richard LIU <liuzhenglaichn@gmail.com>
2017-03-15 23:38:11 +0800

    74 | Search a 2D Matrix | Medium

--- leetcode/74. Search a 2D Matrix/s1.cpp
+++ leetcode/74. Search a 2D Matrix/s1.cpp
@@ -1,23 +1,25 @@
+// OJ: https://leetcode.com/problems/search-a-2d-matrix
+// Auther: github.com/lzl124631x
+// Time: O(log(MN))
+// Space: O(1)
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (matrix.empty() || matrix[0].empty()) return false;
-        int M = matrix.size(), N = matrix[0].size();
-        int L = 0, R = M - 1;
-        while (L <= R) {
-            int MID = (L + R) / 2;
-            if (matrix[MID][0] > target) R = MID - 1;
-            else L = MID + 1;
-        }
-        --L;
-        if (L < 0) return false;
-        vector<int> &row = matrix[L];
-        L = 0, R = N - 1;
-        while (L <= R) {
-            int MID = (L + R) / 2;
-            if (row[MID] >= target) R = MID - 1;
-            else L = MID + 1;
-        }
-        return L >= 0 && L < N && row[L] == target;
+  bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    if (matrix.empty() || matrix[0].empty()) return false;
+    int M = matrix.size(), N = matrix[0].size();
+    int L = 0, R = M - 1, row;
+    while (L <= R) {
+      int mid = (L + R) / 2;
+      if (matrix[mid][0] > target) R = mid - 1;
+      else L = mid + 1;
     }
+    if ((row = R) < 0) return false;
+    L = 0, R = N - 1;
+    while (L <= R) {
+      int mid = (L + R) / 2;
+      if (matrix[row][mid] >= target) R = mid - 1;
+      else L = mid + 1;
+    }
+    return L < N && matrix[row][L] == target;
+  }
 };
--- leetcode/74. Search a 2D Matrix/s2.cpp
+++ leetcode/74. Search a 2D Matrix/s2.cpp
@@ -1,15 +1,17 @@
-// https://discuss.leetcode.com/topic/3227/don-t-treat-it-as-a-2d-matrix-just-treat-it-as-a-sorted-list
+// OJ: https://leetcode.com/problems/search-a-2d-matrix
+// Auther: github.com/lzl124631x
+// Time: O(log(MN))
+// Space: O(1)
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (matrix.empty() || matrix[0].empty()) return false;
-        int M = matrix.size(), N = matrix[0].size();
-        int L = 0, R = M * N - 1;
-        while (L <= R) {
-            int MID = (L + R) / 2;
-            if (matrix[MID / N][MID % N] >= target) R = MID - 1;
-            else L = MID + 1;
-        }
-        return L >= 0 && L <= M * N - 1 && matrix[L / N][L % N] == target;
+  bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    if (matrix.empty() || matrix[0].empty()) return false;
+    int M = matrix.size(), N = matrix[0].size(), L = 0, R = M * N - 1;
+    while (L <= R) {
+      int mid = (L + R) / 2;
+      if (matrix[mid / N][mid % N] >= target) R = mid - 1;
+      else L = mid + 1;
     }
+    return L < M * N && matrix[L / N][L % N] == target;
+  }
 };
