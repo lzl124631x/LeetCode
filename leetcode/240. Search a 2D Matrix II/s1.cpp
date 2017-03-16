6306ae4044c737b299741b7cfe31e67f4f3ac3c8
Richard LIU <liuzhenglaichn@gmail.com>
2017-03-16 22:10:46 +0800

    240 | Search a 2D Matrix II | Medium

--- leetcode/240. Search a 2D Matrix II/s1.cpp
+++ leetcode/240. Search a 2D Matrix II/s1.cpp
@@ -1,13 +1,17 @@
+// OJ: https://leetcode.com/problems/search-a-2d-matrix-ii
+// Auther: github.com/lzl124631x
+// Time: O(M + N)
+// Space: O(1)
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if (matrix.empty() || matrix[0].empty()) return false;
-        int M = matrix.size(), N = matrix[0].size(), i = 0, j = N - 1;
-        while (i < M && j >= 0) {
-            if (matrix[i][j] > target) --j;
-            else if (matrix[i][j] < target) ++i;
-            else return true;
-        }
-        return false;
+  bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    if (matrix.empty() || matrix[0].empty()) return false;
+    int M = matrix.size(), N = matrix[0].size(), i = 0, j = N - 1;
+    while (i < M && j >= 0) {
+      if (matrix[i][j] > target) --j;
+      else if (matrix[i][j] < target) ++i;
+      else return true;
     }
+    return false;
+  }
 };
