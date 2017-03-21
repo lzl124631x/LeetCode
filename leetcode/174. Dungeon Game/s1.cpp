3a3bee76c623810d5bcdeeffd693b19fcf543692
Richard LIU <liuzhenglaichn@gmail.com>
2017-03-21 11:44:45 +0800

    174 | Dungeon Game | Hard

--- leetcode/174. Dungeon Game/s1.cpp
+++ leetcode/174. Dungeon Game/s1.cpp
@@ -1,20 +1,19 @@
+// OJ: https://leetcode.com/problems/dungeon-game
+// Auther: github.com/lzl124631x
+// Time: O(MN)
+// Space: O(1)
 class Solution {
 public:
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        if (dungeon.empty() || dungeon[0].empty()) return 0;
-        int M = dungeon.size(), N = dungeon[0].size();
-        for (int i = M - 1; i >= 0; --i) {
-            for (int j = N - 1; j >= 0; --j) {
-                if (i == M - 1 && j == N - 1) {
-                    dungeon[i][j] = 1 + max(0, -dungeon[i][j]);
-                    continue;
-                }
-                int val = INT_MAX;
-                if (i + 1 < M) val = min(val, max(1, dungeon[i + 1][j] - dungeon[i][j]));
-                if (j + 1 < N) val = min(val, max(1, dungeon[i][j + 1] - dungeon[i][j]));
-                dungeon[i][j] = val;
-            }
-        }
-        return dungeon[0][0];
+  int calculateMinimumHP(vector<vector<int>>& dungeon) {
+    int M = dungeon.size(), N = dungeon[0].size();
+    for (int i = M - 1; i >= 0; --i) {
+      for (int j = N - 1; j >= 0; --j) {
+        int val = INT_MAX;
+        if (i + 1 < M) val = min(val, dungeon[i + 1][j]);
+        if (j + 1 < N) val = min(val, dungeon[i][j + 1]);
+        dungeon[i][j] = max(1, (val == INT_MAX ? 1 : val) - dungeon[i][j]);
+      }
     }
+    return dungeon[0][0];
+  }
 };
--- leetcode/174. Dungeon Game/s2.cpp
+++ leetcode/174. Dungeon Game/s2.cpp
@@ -1,17 +1,21 @@
-// https://discuss.leetcode.com/topic/6912/c-dp-solution
+// OJ: https://leetcode.com/problems/dungeon-game
+// Auther: github.com/lzl124631x
+// Time: O(MN)
+// Space: O(MN)
+// Ref: https://discuss.leetcode.com/topic/6912/c-dp-solution
 class Solution {
 public:
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        if (dungeon.empty() || dungeon[0].empty()) return 0;
-        int M = dungeon.size(), N = dungeon[0].size();
-        vector<vector<int>> dp(M + 1, vector<int>(N + 1, INT_MAX));
-        dp[M][N - 1] = dp[M - 1][N] = 1;
-        for (int i = M - 1; i >= 0; --i) {
-            for (int j = N - 1; j >= 0; --j) {
-                int need = min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
-                dp[i][j] = need <= 0 ? 1 : need;
-            }
-        }
-        return dp[0][0];
+  int calculateMinimumHP(vector<vector<int>>& dungeon) {
+    if (dungeon.empty() || dungeon[0].empty()) return 0;
+    int M = dungeon.size(), N = dungeon[0].size();
+    vector<vector<int>> dp(M + 1, vector<int>(N + 1, INT_MAX));
+    dp[M][N - 1] = dp[M - 1][N] = 1;
+    for (int i = M - 1; i >= 0; --i) {
+      for (int j = N - 1; j >= 0; --j) {
+        int need = min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
+        dp[i][j] = need <= 0 ? 1 : need;
+      }
     }
+    return dp[0][0];
+  }
 };
