c75e83249a6e324d89055e9f2eafc6323ab37f45
Richard LIU <liuzhenglaichn@gmail.com>
2017-03-25 00:19:17 +0800

    78 | Subsets | Medium

--- leetcode/78. Subsets/s1.cpp
+++ leetcode/78. Subsets/s1.cpp
@@ -1,22 +1,25 @@
+// OJ: https://leetcode.com/problems/subsets
+// Auther: github.com/lzl124631x
+// Time: O(2^N)
+// Space: O(N)
 class Solution {
 private:
-    vector<vector<int>> ans;
-    void dfs(vector<int> &nums, int start, vector<int> &sub, int leftLen) {
-        if (leftLen == 0) {
-            ans.push_back(sub);
-            return;
-        }
-        for (int i = start; i <= nums.size() - leftLen; ++i) {
-            sub.push_back(nums[i]);
-            dfs(nums, i + 1, sub, leftLen - 1);
-            sub.pop_back();
-        }
+  vector<vector<int>> ans;
+  void dfs(vector<int> &nums, int start, int len, vector<int> &tmp) {
+    if (!len) {
+      ans.push_back(tmp);
+      return;
     }
-    
+    for (int i = start; i <= nums.size() - len; ++i) {
+      tmp.push_back(nums[i]);
+      dfs(nums, i + 1, len - 1, tmp);
+      tmp.pop_back();
+    }
+  }
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> v;
-        for (int i = 0; i <= nums.size(); ++i) dfs(nums, 0, v, i);
-        return ans;
-    }
+  vector<vector<int>> subsets(vector<int>& nums) {
+    vector<int> tmp;
+    for (int len = 0; len <= nums.size(); ++len) dfs(nums, 0, len, tmp);
+    return ans;
+  }
 };
