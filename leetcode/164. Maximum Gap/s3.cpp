df696aa4bad2c02d2049d231a85d6fd43c4accec
Richard LIU <liuzhenglaichn@gmail.com>
2017-04-08 22:19:54 +0800

    164 | Maximum Gap | Hard

---
+++ leetcode/164. Maximum Gap/s3.cpp
@@ -0,0 +1,13 @@
+// OJ: https://leetcode.com/problems/maximum-gap
+// Auther: github.com/lzl124631x
+// Time: O(NlogN)
+// Space: O(1)
+class Solution {
+public:
+  int maximumGap(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int ans = 0;
+    for (int i = 1; i < nums.size(); ++i) ans = max(ans, nums[i] - nums[i - 1]);
+    return ans;
+  }
+};
