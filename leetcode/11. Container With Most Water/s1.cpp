e1afb2bb669de6522abae89d25310f391d4678ca
Richard LIU <liuzhenglaichn@gmail.com>
2017-04-22 11:38:36 +0800

    11 | Container With Most Water | Medium

--- leetcode/11. Container With Most Water/s1.cpp
+++ leetcode/11. Container With Most Water/s1.cpp
@@ -8,7 +8,7 @@ public:
     int maxVal = min(height[0], height[1]);
     for (int i = 0; i < height.size(); ++i) {
       if (!height[i]) continue;
-      for (int j = i + max(1, maxVal / height[i]); j < height.size(); ++j) {
+      for (int j = i + 1 + maxVal / height[i]; j < height.size(); ++j) {
         maxVal = max(maxVal, min(height[i], height[j]) * (j - i));
       }
     }
