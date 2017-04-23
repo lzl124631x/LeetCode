fd80c42ef440d6e7268289f0adca08bdade360a5
Richard LIU <liuzhenglaichn@gmail.com>
2017-04-23 13:55:41 +0800

    416 | Partition Equal Subset Sum | Medium

--- leetcode/416. Partition Equal Subset Sum/s2.cpp
+++ leetcode/416. Partition Equal Subset Sum/s2.cpp
@@ -1,16 +1,19 @@
+// OJ: https://leetcode.com/problems/partition-equal-subset-sum
+// Auther: github.com/lzl124631x
+// Time: O(NS)
+// Space: O(S) where S the sum of nums.
 class Solution {
+private:
+  bool subsetSum(vector<int> &nums, int S) {
+    vector<int> dp(S + 1);
+    dp[0] = 1;
+    for (int n : nums)
+      for (int i = S; i >= n; --i) dp[i] += dp[i - n];
+    return dp[S];
+  }
 public:
-    bool canPartition(vector<int>& nums) {
-        int target = accumulate(nums.begin(), nums.end(), 0);
-        if (target % 2) return false;
-        target /= 2;
-        vector<bool> dp(target + 1, false);
-        dp[0] = true;
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = target; j >= nums[i]; --j) {
-                dp[j] = dp[j] || dp[j - nums[i]];
-            }
-        }
-        return dp[target];
-    }
+  bool canPartition(vector<int>& nums) {
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    return sum % 2 == 0 && subsetSum(nums, sum / 2);
+  }
 };
