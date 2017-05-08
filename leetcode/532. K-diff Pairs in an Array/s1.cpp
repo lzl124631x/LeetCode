0b159ab6ade65b28b11e4ee3cea3a982cc5be448
Richard LIU <liuzhenglaichn@gmail.com>
2017-05-08 20:58:26 +0800

    399 | Evaluate Division | Medium

--- leetcode/532. K-diff Pairs in an Array/s1.cpp
+++ leetcode/532. K-diff Pairs in an Array/s1.cpp
@@ -1,3 +1,4 @@
+// OJ: https://leetcode.com/problems/k-diff-pairs-in-an-array
 // Author: github.com/lzl124631x
 // Time: O(N)
 // Space: O(N)
@@ -7,11 +8,11 @@ public:
     if (k < 0) return 0;
     unordered_map<int, int> m;
     for (int n : nums) m[n]++;
-      int cnt = 0;
+    int cnt = 0;
     for (auto p : m) {
       if ((!k && p.second > 1)
         || (k && m.count(p.first + k))) ++cnt;
     }
-  return cnt;
-}
+    return cnt;
+  }
 };
