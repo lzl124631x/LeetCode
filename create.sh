ef86722e8419da74d404fefe74aaf31ebf563237
Richard Liu <liuzhenglaichn@gmail.com>
2018-11-16 02:04:08 -0800

    create.sh

--- create.sh
+++ create.sh
@@ -6,8 +6,12 @@ fi
 link=$1
 content=`curl $link`
 if [[ $content =~ questionId:\ \'([0-9]*)\' ]]; then
-    qid=${BASH_REMATCH[1]}
+  qid=${BASH_REMATCH[1]}
+else
+  echo "Unsupported legacy quiz! Abort."
+  exit
 fi
+
 if [[ $content =~ \<title\>(.*)\ \-\ LeetCode\</title\> ]]; then
   title=${BASH_REMATCH[1]}
 fi
