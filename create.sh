ebb43513670451ccf38ab136400c56fb86d5e759
Richard Liu <liuzhenglaichn@gmail.com>
2018-11-21 23:33:06 -0800

    create.sh

--- create.sh
+++ create.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
-if [ $1 -eq '' ]
-  then
-    echo "Quiz # not provided"
-fi
-link=$1
-content=`curl $link`
-if [[ $content =~ questionId:\ \'([0-9]*)\' ]]; then
-  qid=${BASH_REMATCH[1]}
-else
-  echo "Unsupported legacy quiz! Abort."
-  exit
-fi
-
-if [[ $content =~ \<title\>(.*)\ \-\ LeetCode\</title\> ]]; then
-  title=${BASH_REMATCH[1]}
-fi
 cd leetcode/
-folder=$qid". "$title
+folder=$1
 mkdir "$folder"
 cd "$folder"
 cat "../../quiz.js" > README.md
-echo "// OJ: $link
+echo "// OJ: 
 // Author: github.com/lzl124631x
 // Time: O()
 // Space: O()" > s1.cpp
