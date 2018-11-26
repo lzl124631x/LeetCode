6a73e030bc9610f046e14b1c2953c4b00c158c95
Richard Liu <liuzhenglaichn@gmail.com>
2018-11-26 01:02:15 -0800

    926

--- leetcode/926. Flip String to Monotone Increasing/s1.cpp
+++ leetcode/926. Flip String to Monotone Increasing/s1.cpp
@@ -2,15 +2,16 @@
 // Author: github.com/lzl124631x
 // Time: O(N)
 // Space: O(1)
-// Ref: https://leetcode.com/problems/flip-string-to-monotone-increasing/discuss/189751/C%2B%2B-one-pass-DP-solution-0ms-O(n)-or-O(1)-one-line-with-explaination.
 class Solution {
 public:
     int minFlipsMonoIncr(string S) {
-        int ones = 0, ans = 0;
+        int rightZeros = 0, leftOnes = 0;
+        for (char c : S) if (c == '0') rightZeros++;
+        int ans = rightZeros;
         for (char c : S) {
-            if (c == '1') ones++;
-            else ans++;
-            ans = min(ones, ans);
+            if (c == '1') leftOnes++;
+            else rightZeros--;
+            ans = min(ans, rightZeros + leftOnes);
         }
         return ans;
     }
