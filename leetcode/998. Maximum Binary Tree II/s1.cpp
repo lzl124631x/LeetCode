c75aa975facd99e98c43350acb9ce73e30c0bd63
Richard Liu <liuzhenglaichn@gmail.com>
2019-03-16 18:55:26 -0700

    998

---
+++ leetcode/998. Maximum Binary Tree II/s1.cpp
@@ -0,0 +1,21 @@
+// OJ: https://leetcode.com/problems/maximum-binary-tree-ii/
+// Author: github.com/lzl124631x
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+        TreeNode *node = root, *prev = NULL;
+        while (node) {
+            if (val < node->val) {
+                prev = node;
+                node = node->right;
+            } else break;
+        }
+        auto n = new TreeNode(val);
+        if (prev) prev->right = n;
+        else root = n;
+        n->left = node;
+        return root;
+    }
+};
