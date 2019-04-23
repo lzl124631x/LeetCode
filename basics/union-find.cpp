882cd16a953267d4fa23c31c6a522d61c9c772e9
Richard Liu <liuzhenglaichn@gmail.com>
2019-04-22 22:12:07 -0700

    union find

---
+++ basics/union-find.cpp
@@ -0,0 +1,25 @@
+class UnionFind {
+private:
+    vector<int> id, rank;
+    int cnt;
+    int find (int i) {
+        if (id[i] == i) return i;
+        return id[i] = find(id[i]);
+    }
+public:
+    UnionFind(int n) : id(n), rank(n, 0), cnt(n) {
+        for (int i = 0; i < n; ++i) id[i] = i;
+    }
+    void connect(int i, int j) {
+        int p = find(i), q = find(j);
+        if (p == q) return;
+        if (rank[p] > rank[q]) id[p] = q;
+        else {
+            id[q] = p;
+            if (rank[p] == rank[q]) rank[p]++;
+        }
+        --cnt;
+    }
+    bool connected(int i, int j) { return find(i) == find(j); }
+    int getCount() { return cnt; }
+};
