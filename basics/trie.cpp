56ab739eac7257eecaa29fcd588c1c025eb688a6
Richard Liu <liuzhenglaichn@gmail.com>
2019-05-18 01:23:40 -0700

    trie

---
+++ basics/trie.cpp
@@ -0,0 +1,36 @@
+class TrieNode {
+public:
+    TrieNode *next[26] = {};
+    int count = 0;
+};
+
+class Trie {
+private:
+    TrieNode root;
+    TrieNode* searchToNode(string &prefix) {
+        auto node = &root;
+        for (char c : prefix) {
+            if (!node->next[c - 'a']) return NULL;
+            node = node->next[c - 'a'];
+        }
+        return node;
+    }
+public:
+    void insert(string word) {
+        auto node = &root;
+        for (char c : word) {
+            if (!node->next[c - 'a']) node->next[c - 'a'] = new TrieNode();
+            node = node->next[c - 'a'];
+        }
+        node->count++;
+    }
+
+    bool search(string word) {
+        auto node = searchToNode(word);
+        return node && node->count;
+    }
+
+    bool startsWith(string prefix) {
+        return searchToNode(prefix);
+    }
+};
