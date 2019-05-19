19c41f3d2e50a492ec9ad504d0cf296b29c7868d
Richard Liu <liuzhenglaichn@gmail.com>
2019-05-19 13:47:43 -0700

    122

--- leetcode/122. Best Time to Buy and Sell Stock II/s1.cpp
+++ leetcode/122. Best Time to Buy and Sell Stock II/s1.cpp
@@ -6,9 +6,7 @@ class Solution {
 public:
   int maxProfit(vector<int>& prices) {
     int ans = 0;
-    for (int i = 1; i < prices.size(); ++i) {
-      ans += max(prices[i] - prices[i - 1], 0);
-    }
+    for (int i = 1; i < prices.size(); ++i) ans += max(prices[i] - prices[i - 1], 0);
     return ans;
   }
 };
