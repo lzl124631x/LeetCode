46630064a06684553f9c3e4f9257c26915e20440
Richard Liu <liuzhenglaichn@gmail.com>
2019-05-29 21:21:20 -0700

    791

--- leetcode/791. Custom Sort String/s1.cpp
+++ leetcode/791. Custom Sort String/s1.cpp
@@ -6,9 +6,7 @@ class Solution {
 public:
     string customSortString(string S, string T) {
         int m[26] = { 0 };
-        for (int i = 0; i < S.size(); ++i) {
-            m[S[i] - 'a'] = i;
-        }
+        for (int i = 0; i < S.size(); ++i) m[S[i] - 'a'] = i;
         sort(T.begin(), T.end(), [&](const char a, const char b) {
             return m[a - 'a'] < m[b - 'a'];
         });
