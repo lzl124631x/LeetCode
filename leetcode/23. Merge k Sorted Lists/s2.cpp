819a3dd7ef5f8a9be5e3078f54a6742ed972d505
Richard Liu <liuzhenglaichn@gmail.com>
2019-06-01 18:36:15 -0700

    23

--- leetcode/23. Merge k Sorted Lists/s2.cpp
+++ leetcode/23. Merge k Sorted Lists/s2.cpp
@@ -17,12 +17,10 @@ private:
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if (lists.empty()) return NULL;
-        int N = lists.size();
-        while (N > 1) {
+        for (int N = lists.size(); N > 1; N = (N + 1) / 2) {
             for (int i = 0; i < N / 2; ++i) {
                 lists[i] = mergeTwoLists(lists[i], lists[N - 1 - i]);
             }
-            N = (N + 1) / 2;
         }
         return lists[0];
     }
