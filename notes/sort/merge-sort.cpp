b5aaba3651e0a9f5101f072e32771bdc31b4a556
Richard Liu <liuzhenglaichn@gmail.com>
2020-01-19 00:34:15 -0800

    merge sort

--- notes/sort/merge-sort.cpp
+++ notes/sort/merge-sort.cpp
@@ -4,20 +4,22 @@
 // Space: O(N)
 class Solution {
 private:
-    vector<int> tmp;
-    void mergeSort(vector<int> &nums, int start, int end) {
+    void _mergeSort(vector<int> &nums, vector<int> &tmp, int start, int end) {
         if (start + 1 >= end) return;
         int mid = (start + end) / 2;
-        mergeSort(nums, start, mid);
-        mergeSort(nums, mid, end);
+        _mergeSort(nums, tmp, start, mid);
+        _mergeSort(nums, tmp, mid, end);
         for (int i = start, j = mid, k = 0; i < mid || j < end; ++k) {
             tmp[k] = (i >= mid || (j < end && nums[j] < nums[i])) ? nums[j++] : nums[i++];
         }
         for (int i = start; i < end; ++i) nums[i] = tmp[i - start];
     }
+    void mergeSort(vector<int> &nums, int start, int end) {
+        vector<int> tmp(end - start);
+        _mergeSort(nums, tmp, start, end);
+    }
 public:
     vector<int> sortArray(vector<int>& nums) {
-        tmp = vector<int>(nums.size());
         mergeSort(nums, 0, nums.size());
         return nums;
     }
