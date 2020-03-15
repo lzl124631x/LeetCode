b22313a27e5b9d4bb8d17169c81d41c95c9c9938
Richard Liu <liuzhenglaichn@gmail.com>
2020-03-15 15:00:44 -0700

    testcase

--- testcase.js
+++ testcase.js
@@ -1,1 +1,2 @@
+$('.source-content').remove(); // For problem in contest, there is a hidden `.source-content`. Ignore it.
 copy($$('strong').filter(x => x.innerText.trim() === "Input:").map(x => x.nextSibling.textContent.split('\n')[0].trim()).map(x => x.replace(/(\,\ )?\w+\ =\ /g, '\n').trim()).join('\n'))
