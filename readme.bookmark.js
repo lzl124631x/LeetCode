c477643bcebb2f12db2f7c881047733f000bc2d9
Richard Liu <liuzhenglaichn@gmail.com>
2020-03-21 13:55:04 -0700

    bookmark

---
+++ readme.bookmark.js
@@ -0,0 +1,16 @@
+!function(d) {
+    var $=d.querySelector.bind(d);
+    function copy(str) { // Chrome Console API is not available :( Have to use this way.
+        const el = d.createElement('textarea');
+        el.value = str;
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        d.body.appendChild(el);
+        el.select();
+        d.execCommand('copy');
+        d.body.removeChild(el);
+    }
+    title = $('[data-cy=question-title]').textContent.trim();
+    copy(`${title.replace('.', ' |')} | ${$('[diff]').textContent} | [Solution](leetcode/${encodeURIComponent(title.trim())})`)
+}(document)
