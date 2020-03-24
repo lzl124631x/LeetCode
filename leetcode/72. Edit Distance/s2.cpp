65032ae78f71b113bfc4ae4e3d210bbce2b37ad8
Richard Liu <liuzhenglaichn@gmail.com>
2020-03-23 22:46:56 -0700

    72

--- leetcode/72. Edit Distance/s2.cpp
+++ leetcode/72. Edit Distance/s2.cpp
@@ -7,17 +7,14 @@ public:
     int minDistance(string A, string B) {
         int M = A.size(), N = B.size();
         if (M < N) swap(A, B), swap(M, N); 
-        vector<int> dp(N + 1, INT_MAX);
+        vector<vector<int>> dp(2, vector<int>(N + 1, INT_MAX));
         for (int i = 0; i <= M; ++i) {
-            int prev;
             for (int j = 0; j <= N; ++j) {
-                int cur = dp[j];
-                if (i == 0 || j == 0) dp[j] = i + j;
-                else if (A[i - 1] == B[j - 1]) dp[j] = prev;
-                else dp[j] = 1 + min({ dp[j], dp[j - 1], prev });
-                prev = cur;
+                if (i == 0 || j == 0) dp[i % 2][j] = i + j;
+                else if (A[i - 1] == B[j - 1]) dp[i % 2][j] = dp[(i - 1) % 2][j - 1];
+                else dp[i % 2][j] = 1 + min({ dp[(i - 1) % 2][j], dp[i % 2][j - 1], dp[(i - 1) % 2][j - 1] });
             }
         }
-        return dp[N];
+        return dp[M % 2][N];
     }
 };
