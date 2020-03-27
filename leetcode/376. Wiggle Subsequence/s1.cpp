fadb5c330174a45bed22d56cea3a8503c9b73171
Richard Liu <liuzhenglaichn@gmail.com>
2020-03-26 22:35:17 -0700

    376

--- leetcode/376. Wiggle Subsequence/s1.cpp
+++ leetcode/376. Wiggle Subsequence/s1.cpp
@@ -1,24 +1,16 @@
+// OJ: https://leetcode.com/problems/wiggle-subsequence/
+// Author: github.com/lzl124631x
+// Time: O(N)
+// Space: O(1)
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        if (nums.size() <= 1) return nums.size();
-        vector<vector<int>> dp(nums.size(), vector<int>(2, 0));
-        dp[0][0] = dp[0][1] = 1;
-        int maxLen = 1;
-        for (int i = 1; i < nums.size(); ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] - nums[j] > 0) {
-                    dp[i][0] = max(dp[i][0], dp[j][1] + 1);
-                    maxLen = max(dp[i][0], maxLen);
-                } else if (nums[i] - nums[j] < 0) {
-                    dp[i][1] = max(dp[i][1], dp[j][0] + 1);
-                    maxLen = max(dp[i][1], maxLen);
-                }
-            }
+        if (nums.empty()) return 0;
+        int inc = 1, dec = 1, N = nums.size();
+        for (int i = 1; i < N; ++i) {
+            if (nums[i] > nums[i - 1]) inc = dec + 1;
+            else if (nums[i] < nums[i - 1]) dec = inc + 1;
         }
-        return maxLen;
+        return max(inc, dec);
     }
 };
-
-// Space: O(n)
-// Time: O(n^2)
