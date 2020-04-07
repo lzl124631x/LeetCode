b4ccbe50f90db821ebeb414fda673a3b2339264b
Richard Liu <liuzhenglaichn@gmail.com>
2020-04-07 03:35:49 -0700

    289

--- leetcode/289. Game of Life/s1.cpp
+++ leetcode/289. Game of Life/s1.cpp
@@ -3,21 +3,19 @@
 // Time: O(N^2)
 // Space: O(1)
 class Solution {
-    vector<vector<int>> *A;
     int M, N;
-    bool alive(int x, int y) {
-        return x >= 0 && y >= 0 && x < M && y < N && ((*this->A)[x][y] & 1);
+    inline bool alive(vector<vector<int>> &A, int x, int y) {
+        return x >= 0 && y >= 0 && x < M && y < N && A[x][y] & 1;
     }
 public:
     void gameOfLife(vector<vector<int>>& A) {
         if (A.empty() || A[0].empty()) return;
         int dx[8] = {-1,-1,0,1,1,1,0,-1}, dy[8] = {0,1,1,1,0,-1,-1,-1};
-        this->A = &A;
         M = A.size(), N = A[0].size();
         for (int i = 0; i < M; ++i) {
             for (int j = 0; j < N; ++j) {
                 int cnt = 0;
-                for (int k = 0; k < 8; ++k) cnt += alive(i + dx[k], j + dy[k]);
+                for (int k = 0; k < 8; ++k) cnt += alive(A, i + dx[k], j + dy[k]);
                 if ((A[i][j] && cnt == 2) || cnt == 3) A[i][j] |= 2;
             }
         }
@@ -27,4 +25,4 @@ public:
             }
         }
     }
-}
+};
