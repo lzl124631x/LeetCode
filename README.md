15a2033178a65b265b479e2df2f4f9c7eddf4664
Richard Liu <liuzhenglaichn@gmail.com>
2020-04-28 21:22:44 -0700

    first unique number

--- README.md
+++ README.md
@@ -815,6 +815,7 @@ Please give this repo a :star: if it inspires you. Thanks. :blush:
 1423 | Maximum Points You Can Obtain from Cards | Medium | [Solution](leetcode/1423.%20Maximum%20Points%20You%20Can%20Obtain%20from%20Cards)
 1424 | Diagonal Traverse II | Medium | [Solution](leetcode/1424.%20Diagonal%20Traverse%20II)
 ? | Leftmost Column with at Least a One | ? | [Solution](leetcode/Leftmost%20Column%20with%20at%20Least%20a%20One)
+? | First Unique Number | ? | [Solution](leetcode/First%20Unique%20Number)
 
 # License
 
---
+++ leetcode/First Unique Number/s1.cpp
@@ -0,0 +1,32 @@
+// OJ: https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/531/week-4/3313/
+// Author: github.com/lzl124631x
+// Time:
+//    FirstUnique: O(N)
+//    showFirstUnique: O(1)
+//    add: O(1)
+// Space: O(N)
+class FirstUnique {
+    list<int> data;
+    typedef list<int>::iterator iter;
+    unordered_map<int, iter> m;
+    unordered_set<int> s;
+public:
+    FirstUnique(vector<int>& nums) {
+        for (int n : nums) add(n);
+    }
+    
+    int showFirstUnique() {
+        return data.size() ? data.front() : -1;
+    }
+    
+    void add(int value) {
+        if (s.count(value)) return;
+        if (m.count(value)) {
+            data.erase(m[value]);
+            s.insert(value);
+        } else {
+            data.push_back(value);
+            m[value] = prev(data.end());
+        }
+    }
+};
